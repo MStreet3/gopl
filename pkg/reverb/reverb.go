5194bc7c99e9501c5112422b660b9c530164538e
Michael Street <5597260+MStreet3@users.noreply.github.com>
2022-03-26 18:17:08 -0400

    graceful shutdown with channels

--- pkg/reverb/reverb.go
+++ pkg/reverb/reverb.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -26,34 +29,110 @@ func handleConn(c net.Conn) {
 	c.Close()
 }
 
+func handleConnStream(stop chan int, stream chan net.Conn) {
+	for {
+		select {
+		case <-stop:
+			log.Println("closing reverb client connection handler")
+			return
+
+		case conn := <-stream:
+			log.Println("handling connection")
+			go handleConn(conn)
+		}
+	}
+}
+
+func serve(stop chan int, listener net.Listener) {
+	log.Println("reverb server started...")
+	var (
+		wg         sync.WaitGroup
+		done       = make(chan int)
+		connStream = make(chan net.Conn)
+	)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		handleConnStream(stop, connStream)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	for {
+		select {
+		case <-done:
+			log.Println("reverb server stopped")
+			return
+
+		case <-stop:
+			log.Println("stopping reverb server")
+			continue
+
+		default:
+			log.Println("awaiting connections")
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			connStream <- conn
+		}
+	}
+}
+
 func main() {
 
 	var (
-		port int
-		addr string
+		port      int
+		addr      string
+		wg        sync.WaitGroup
+		stop      = make(chan int)
+		done      = make(chan int)
+		interrupt = make(chan os.Signal, 1)
 	)
 
-	// command line args
+	// Notify main of any interruptions
+	signal.Notify(interrupt, os.Interrupt)
+
+	// Fetch command line args
 	flag.IntVar(&port, "port", 8000, "port to listen for connections on")
 	flag.Parse()
 	addr = fmt.Sprintf("localhost:%d", port)
 
+	// Create a new listener
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("server started...")
+	// Launch server
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		serve(stop, listener)
+	}()
+
+	// Launch shutdown watcher
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	// Handle graceful shutdown
 	for {
-		log.Println("awaiting connections")
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Print(err)
-			continue
+		select {
+		case <-done:
+			log.Println("shutdown complete, goodbye")
+			return
+		case <-interrupt:
+			log.Println("starting graceful shutdown")
+			listener.Close()
+			close(stop)
 		}
-
-		log.Println("handling connection")
-		go handleConn(conn)
 	}
-
 }
